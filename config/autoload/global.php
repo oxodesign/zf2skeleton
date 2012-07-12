86aaeafde07e5a09d886aaff18a53e44f88c59ac
Flamur Mavraj <info@oxodesign.no>
2012-07-12 11:32:50 +0200

    Standard skeleton without the module

---
+++ config/autoload/global.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: oxodesign
+ * Date: 12.07.12
+ * Time: 11:21
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ config/autoload/local.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: oxodesign
+ * Date: 12.07.12
+ * Time: 11:22
+ * To change this template use File | Settings | File Templates.
+ */
